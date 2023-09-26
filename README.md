eaf4a0d2a73ea5b27fa56b92a8be303287ecbef1
jx0906 <sandy_hong@live.com>
2023-09-27 00:55:29 +0800

    tried my best. still doesn't work. will trbsht ltr

--- README.md
+++ README.md
@@ -1,1 +1,115 @@
-# tic-tac-toe
+## Steps to Getting Started on a Browser Game (extracted from SEI Course Material Guidelines)
+
+1. **Analyze the app's functionality**: The app's features, from the user's point of view, should be described using _User Stories_.
+
+    As a user, I want to be able to play Tic Tac Toe (ie, be able to mark out X and O on a 9-grid box).
+    As a user, I would like feedback on when it's my turn and who has won the game.
+
+2. **Think about the overall design (look & feel) of the app**
+
+    Take the users (audience) of the app into consideration when determining the overall look and feel the app should have. Should the app have a clean/minimalist UI (current trend),  or should it be themed to match the app's purpose?
+
+3. **Wireframe the UI**
+
+    Wireframes provide a blueprint for the HTML & CSS. Wireframes also help reveal an application's state (data) and functionality.
+
+4. **Pseudocode**
+
+   Pseudocode outlines the app's logic using plain language. It provides a road map to writing the code itself. For example, pseudocoding the logic for when a player makes a move, checking if the game has been won, etc., will prove to be helpful when writing the actual code.
+
+5. **Identify the application's state (data)**
+
+    What does the application need to "remember" throughout its execution?
+
+    Use the wireframe and pseudocode to help identify what state needs to be tracked.
+
+6. **Set up the project**
+
+- Create a directory for the project in the `~/code` folder.
+
+- Create the starting folders/files within the project folder:
+
+  - **index.html**
+  - **css/main.css**
+  - **js/main.js**
+
+- Create the HTML boilerplate within **index.html** using `![tab]`.
+
+- Link **main.css** in the `<head>`.
+
+- Add a `<script>` tag to load the **main.js** in the `<head>`.
+
+  Be sure to use the `defer` attribute to ensure that the DOM is ready before the script runs.
+  
+  Here you go:
+
+    ```html
+    <script defer src="js/main.js"></script>
+    ```
+
+7. **Create a local repo**
+
+- Make your project a local repo with `git init`.
+
+- Next, think about the name for your remote repo on GitHub - using a name that represents your choice of game, e.g., "blackjack", is better than something like "project-1".  It's also recommended that the name of the repo and the project directory match.
+
+- Create your remote repo in your **PERSONAL** GitHub account - be sure **NOT** to check the "Initialize this repository with a README" checkbox (you'll want to `touch README.md` locally).
+  
+- Run the terminal command that the GitHub instructions provides to add the remote in your local repo.  It will look like this:
+
+    ```bash
+    git remote add origin <the URL to your repo>
+    ```
+  
+- Make your first commit:  `git add -A`, then `git commit -m "Initial commit"`
+  
+- Push the commit to the repo for the **first time** using:<br />`git push -u origin main`.  
+
+- Future pushes can now be made using just<br /> `git push`.
+
+8. **Organize the app's JS into sections**
+
+ Copy/paste the following comment headings to help you organize your app's code:
+
+```js
+ /*----- constants -----*/
+
+
+ /*----- state variables -----*/
+
+
+ /*----- cached elements  -----*/
+
+
+ /*----- event listeners -----*/
+
+
+ /*----- functions -----*/
+
+```
+
+9. **Code away!**
+
+- Start with some HTML in index.html for the basic layout of the UI. If an element's content is going to come from the `render` function, you may want to temporarily include mocked content in the HTML to help with layout and styling. However, once the content is being provided by the `render()` function, you should remove the mocked content from index.html.
+
+- Declare, but don't initialize, the application-wide state variables. The initialization of the variables to their "initial" state should be done within an `initialize`, or similarly named function, e.g., `init`.
+
+- Write that `initialize` function.
+
+- Invoke `initialize()` to "kick off" the app.
+
+- Now that the `initialize` function has initialized the state variables, the last line in `initialize` should be `render();` to render that state to the DOM for the first time.
+
+- Stub up that `render()` function.
+  
+- As a reminder, after state has been updated in an event listener, the last line of code in the event listener function should be a call to `render();` again, to render the state to the DOM.
+
+- Register event listeners - be sure to use event delegation!
+
+- Code the event listener(s). This is where most of the app's logic will exist.
+
+11. **Make frequent git commits**
+
+At a minimum, commit after each "milestone" or feature implementation.
+
+12. **Have fun!**
---
+++ script.js
@@ -0,0 +1,220 @@
+ // constants
+
+ const winCombi = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+];
+
+ // variables
+let player1 = {
+    name: "Player 1",
+    sym: 'X'
+};
+
+let player2 = {
+    name: "Player 2",
+    sym: 'O'
+};
+
+// state of game board - will change, so its a variable
+let stateBoard = [];
+
+let gameState = {
+    stateBoard,
+    turn: '',
+    playerSym: ''
+};
+
+
+ // cached elements -This term emphasizes the idea of storing a reference to a DOM element in a variable to improve performance 
+ // by avoiding repeated DOM queries. When you cache an element, you are essentially setting it as a variable.
+const gameBoard = document.getElementById('game-board');
+const statusBoard = document.getElementById('status-board');
+const playerTurn = document.getElementById('player-turn');
+const resetButton = document.getElementById('reset-button');
+
+ //---- event listeners
+ gameBoard.addEventListener('click', handleMove);
+ resetButton.addEventListener('click', initialize);
+
+ //----- functions
+
+ //function to create 3x3 board - could have coded directly in html but wanna try this potentially more flexible mtd
+ //let user define "3" if intending to create dynamic board - see tips.txt in sample_tictactoe folder
+ function createBoard() {
+
+    for (let i = 0; i < 3; i++) {
+        //this will lead to the same effect as having </div><div> in the .game-board HTML elem to facilitate css styling, js functions
+        const row = document.createElement('div');
+        row.className = 'row';
+
+        for (let j = 0; j < 3; j++) {
+            const cell = document.createElement('div');
+            cell.className = 'cell';
+
+            //These lines set a custom data attribute called "row" and "col" on each cell element. It stores the row index i and col index j.
+            //chose this method over setting 9 "rows" to prevent having to deal with flexbox woes (ie, how to get 9 boxes to line up as 3x3 box?)
+            cell.dataset.row = i;
+            cell.dataset.col = j;
+
+            //The cell element created in the inner loop is appended as a child to the row element, building the structure of the game board.
+            row.appendChild(cell);
+        }
+
+        gameBoard.appendChild(row);
+    }
+};
+
+function initialize() {
+    gameBoard.innerHTML = ''; //clear board else it will just keep appending another 3x3 grid
+    createBoard();
+    statusBoard.textContent = "Game starts now!"
+    playerTurn.textContent = "Player 1 first"
+    gameState = {
+        stateBoard: ['','','','','','','','',''],
+        turn : player1.name, //note with obj, we need to refer their keys (eg, turn) to another object's key (eg, player1.name) instead of simply 
+        // calling the object (eg, turn: player1) else the code wouldn't work
+        playerSym: player1.sym
+    };
+    setupStateBoard();
+
+}
+
+function setupStateBoard() {
+// function needs to be able to recog the col and rows and store it as an array in stateGame so each game box will be identified as 1.1, 1.2, 1.3. winning combo has to reflect this?
+// this is because I had opted to use a row, col dynamic method, instead of recognising them as 9 rows, ie, sq1, sq2, sq3 etc.
+// Iterate through the elements with "row" class-name within the game-board; store the row id data in rArray
+const rows = gameBoard.getElementsByClassName('row');
+// rows.length to determine the number of elements that were found using the getElementsByClassName method under the gameBoard element.
+for (let i = 0; i < rows.length; i++) {
+    const r = rows[i];
+    const rArray = [];
+
+    // Iterate through the cells within each row; extract the values of "data-row" and "data-col" attributes, 
+    // parse them into integers, and creates an object (rData) with this information.
+
+    const cells = r.getElementsByClassName('cell');
+    for (let j = 0; j < cells.length; j++) {
+        const c = cells[j];
+        const rData = {
+            row: parseInt(c.getAttribute('data-row')),
+            col: parseInt(c.getAttribute('data-col'))
+        };
+
+        // Push the rData object into rArray for the current row
+        rArray.push(rData);
+    }
+
+    // Add the row's data to the main stateBoard array by pushing the rArray info into stateBoard.
+    //stateBoard will now be a 2D array, where each element corresponds to data-row and data-col values of the r and c. 
+    // to access a specific element in the array, i will need to call "stateBoard[i][j]", where i and j= the value of the row and col i want to call.
+    stateBoard.push(rArray);
+}
+
+console.log(stateBoard); 
+}
+// console.log(stateBoard) gives: 
+//  [[ { row: 0, col: 0 }, { row: 0, col: 1 }, { row: 0, col: 2 } ],
+//  [ { row: 1, col: 0 }, { row: 1, col: 1 }, { row: 1, col: 2 } ],
+//  [ { row: 2, col: 0 }, { row: 2, col: 1 }, { row: 2, col: 2 } ]]
+
+    function handleMove(evt) {
+        let symToUpdate = gameState.playerSym;
+        const chosenCell = evt.target;
+        
+        // check if chosen cell is empty
+        if (chosenCell.textContent == '') {
+            //update chosen cell with playerSym
+            chosenCell.textContent = symToUpdate;
+            //update gameState object, ie, stateBoard, turn, playerSym
+            // a. update stateBoard array by parsing the dataset.row and dataset.col attributes of the clicked cell to get it's index for updating stateBoard
+            // impt to do this so the cache is kept separately in the datastore ("stateBoard") and not the DOM object (ie, chosenCell through .textContent)
+            gameState.stateBoard[parseInt(chosenCell.dataset.row)][parseInt(chosenCell.dataset.col)] = symToUpdate;
+
+            //check for winner
+            checkWin(symToUpdate);
+
+            // b. update turn by checking current player name (ie, gameState.turn). back part starting from gameState.turn === player1.name represents the condition
+            // alr. ie, check if gameState.turn = player 1  
+            // "?" = if truthy, gameState.turn should = player 2 next, so set current player to player2.name; else, set to player1.name
+            gameState.turn = gameState.turn === player1.name ? player2.name : player1.name;
+
+            // c. update the playerSym as well
+            gameState.playerSym = gameState.playerSym === player1.sym ? player2.sym : player1.sym;
+            
+            // for debugging - to check if on track
+            playerTurn.textContent = `over to ${gameState.turn} now!`; 
+
+        }
+        else { // ie, evt.target.value !== ''
+            statusBoard.textContent = "The cell is already taken. Pls choose another one.";
+        }
+        render();
+    }
+    
+    function checkWin (pSym) {
+        for (combi of winCombi) {
+            // get value of array use 'of'; get key/index use 'in' for array
+            // use 'in' for objects; 'of' will return undefined
+            // so here combi will be returned as [0,1,2]
+            const [a,b,c] = combi; 
+            
+            // cannot just check combi[0] === pSymb because winCombi stores indexes of potential winning combi. If you use combi[0] === pSym, it would check whether the index
+            // of the first cell in the combination is equal to pSym. This comparison doesn't directly relate to the symbols in the cells, and it wouldn't accurately determine
+            // if a player has won.
+
+            // so we need to establish variables for the content within combi to compare with the tictactoe board content (X, O) input.
+            // here, by setting [a,b,c] = combi, we are telling the code that "a" rep an index within the winCombi array, ie, it rep one of the indices within a potential winning
+            // combination.
+            // Now we will need to calculate the row and column indices for a cell based on the winning combi then extract the symbol (X or O) from stateBoard for that cell
+            // Aft that's done, we use the symbol to check if a player has won by comparing it with the symbols in the other cells for the winning combi.
+
+            const symbolA = gameState.stateBoard[Math.floor(a / 3)][a % 3];
+            const symbolB = gameState.stateBoard[Math.floor(b / 3)][b % 3];
+            const symbolC = gameState.stateBoard[Math.floor(c / 3)][c % 3];
+
+            // Math.floor(a / 3) is used to calculate the row index for the corresponding cell in the stateBoard. Since there are 3
+            // cells in each row (a 3x3 grid), dividing a by 3 and rounding down with Math.floor will give us the row index.
+            
+            // a % 3 is used to calculate the column index for the corresponding cell in the stateBoard. The % (modulo) operator gives you the remainder when 
+            // a is divided by 3, effectively cycling through values 0, 1, and 2, which correspond to the three columns in each row.
+
+            if (symbolA === pSym && symbolB === pSym && symbolC === pSym) {
+                    statusBoard.textContent = `${gameState.turn} has won!`;}
+        };
+        statusBoard.textContent = "No winner has been declared but we should be getting closer!";
+    }
+
+    function render() {
+        renderBoard();
+    }
+
+    function renderBoard() {
+        const rows = gameBoard.getElementsByClassName('row');
+        // rows.length to determine the number of elements that were found using the getElementsByClassName method under the gameBoard element.
+        for (let i = 0; i < rows.length; i++) {
+            const r = rows[i];
+            const rArray = [];
+        
+            // Iterate through the cells within each row; extract the values of "data-row" and "data-col" attributes, 
+            // parse them into integers, and creates an object (rData) with this information.
+        
+            const cells = r.getElementsByClassName('cell');
+            for (let j = 0; j < cells.length; j++) {
+                const c = cells[j];
+                const rData = {
+                    row: parseInt(c.getAttribute('data-row')),
+                    col: parseInt(c.getAttribute('data-col'))
+                };
+        
+                // Push the rData object into rArray for the current row
+                rArray.push(rData);
+            }
+            stateBoard.push(rArray);
+        }};
